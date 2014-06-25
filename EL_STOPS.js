4432ff69a39a62f681bef220ac17a6f47e6db9d8
Jeremy Moore <jeremy@moremooredesign.com>
2014-06-24 23:41:53 -0400

    working on callback for AJAX

--- EL_STOPS.js
+++ EL_STOPS.js
@@ -3,32 +3,21 @@
 function EL_STOPS(){
 
 	var endPoint = "http://data.cityofchicago.org/resource/8pix-ypme.json";
-	var sR = new Object();
+	var data = new Object();
 
 	EL_STOPS.prototype.listing = function() {
-
+		console.log("listing");
 		var getData = $.ajax({
 						url: endPoint,
 						type: "GET",
-						dataType: "json"
+						dataType: "json",
+						complete: function (data) {success(data);}
+
 		});
 
-		getData.done( function(response){
-
-			sR.response = response;
-			console.log(sR);
-
-		});//end of response
-
-		return this;
-
-	}
-
-	EL_STOPS.prototype.result = function() {
-
-		console.log("result of stops");
-
-		return sR;
+		function success(data){
+			console.log(data);
+		}
 
 	}
 
